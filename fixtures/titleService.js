0336e6d544acd27b649af2b795507104363f22f4
bcabanes <contact@benjamincabanes.com>
2015-07-26 16:23:30 -0400

    [FIXTURES] add new fixtures

---
+++ fixtures/titleService.js
@@ -0,0 +1,34 @@
+angular.module('titleService', [])
+
+  .factory('titleService', function ($state, $document, $filter, $translate) {
+
+    $state.state('home', {
+      url: '/home',
+      data: {
+        pageTitleSimple: /* i18nextract */'mySingleQ\'uotedCommentTranslation \'',
+        pageTitleDouble: /* i18nextract */"myDoubleQuotedCommentTranslation \"e\"s\""
+      }
+    });
+
+    $filter('translate')('JavascriptFilter 1/2 with var "{name}"', {name: 'name'});
+    $filter('translate')('JavascriptFilter 2/2 without var');
+
+    $translate('JavascriptServiceSimpleQuote 1/2 with var "{name}".', {name: 'name'});
+    $translate("JavascriptServiceDoubleQuote 2/2 with var \"{name}\".", {name: 'name'});
+    $translate.instant('JavascriptServiceInstantSimpleQuote 1/2 with var "{name}".', {name: 'name'});
+    $translate.instant("JavascriptServiceInstantDoubleQuote 2/2 with var \"{name}\".", {name: 'name'});
+
+      $translate([
+        'JavascriptServiceArraySimpleQuote 1/2 without var.',
+        'JavascriptServiceArraySimpleQuote 2/2 without var.'
+      ]);
+
+      $translate(["JavascriptServiceArrayDoubleQuote 1/2 without var.", "JavascriptServiceArrayDoubleQuote 2/2 without var."]);
+
+    var titleService = {
+    };
+
+    return titleService;
+  })
+
+;
